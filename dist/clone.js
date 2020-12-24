d7dfc4608d9cd9b46c144f5a4252cc266bc684fb
Zim <zimtsui@gmail.com>
2020-12-24 21:16:56 +0800

    dev

--- dist/clone.js
+++ dist/clone.js
@@ -1,8 +1,8 @@
-import { clone as copy } from 'interfaces';
+import { clone as copy, reviver, } from 'interfaces';
 import { OrderbookManager } from './orderbook-manager';
 export function clone(x) {
     if (x instanceof OrderbookManager)
-        return JSON.parse(JSON.stringify(x));
+        return JSON.parse(JSON.stringify(x), reviver);
     return copy(x);
 }
 export default clone;
--- src/clone.ts
+++ src/clone.ts
@@ -1,9 +1,12 @@
-import { clone as copy } from 'interfaces';
+import {
+    clone as copy,
+    reviver,
+} from 'interfaces';
 import { OrderbookManager } from './orderbook-manager';
 
 export function clone<T>(x: T): T {
     if (x instanceof OrderbookManager)
-        return JSON.parse(JSON.stringify(x));
+        return JSON.parse(JSON.stringify(x), reviver);
     return copy(x);
 }
 
