f174d3ab01d28c12defa0aaa433aa18ba3b4334b
Zim <zimtsui@gmail.com>
2021-08-03 02:45:38 +0800

    built

--- build/4.3-others.d.ts
+++
@@ -1,8 +0,0 @@
-import { Texchange as Parent, Events } from './4.2-taken';
-import { OpenOrder } from './interfaces';
-declare abstract class Texchange extends Parent {
-    protected settle(): void;
-    /** @override */
-    protected cancelOpenOrder(order: OpenOrder): OpenOrder;
-}
-export { Texchange, Events, };
--- build/5.2-taken.d.ts
+++
@@ -1,8 +0,0 @@
-import { Texchange as Parent, Events } from './5.1-making';
-import { UnidentifiedTrade, OpenMaker } from './interfaces';
-import Big from 'big.js';
-declare abstract class Texchange extends Parent {
-    /** @override */
-    protected uTradeTakesOpenMaker(uTrade: UnidentifiedTrade, maker: OpenMaker): Big;
-}
-export { Texchange, Events, };
