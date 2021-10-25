a48cde449de75c4f8434ea5ebde3fd34e18c584b
Zim <zimtsui@gmail.com>
2021-10-26 00:56:18 +0800

    dev

--- build/methods/ordering.d.ts
+++
@@ -1,8 +0,0 @@
-import { OpenOrder } from '../interfaces';
-import { Core } from '../core';
-export declare class MethodsOrdering {
-    private core;
-    constructor(core: Core);
-    cancelOpenOrder(order: OpenOrder): OpenOrder;
-    makeOpenOrder(order: OpenOrder): OpenOrder;
-}
