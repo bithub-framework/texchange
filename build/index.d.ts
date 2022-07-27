97157c1be87fa3c8b3aed10a5d01254067ae5b06
Zim <zimtsui@gmail.com>
2022-07-27 15:09:40 +0800

    export

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,2 +1,8 @@
 export { Container as BaseContainer } from './injection/container';
 export { Container as DefaultContainer } from './injection/default/container';
+export * from './interfaces/database-orderbook';
+export * from './interfaces/database-trade';
+export * from './facades.d/admin';
+export * from './facades.d/user-market';
+export * from './facades.d/user-account';
+export * from './texchange';
--- build/index.js
+++ build/index.js
@@ -1,8 +1,24 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DefaultContainer = exports.BaseContainer = void 0;
 var container_1 = require("./injection/container");
 Object.defineProperty(exports, "BaseContainer", { enumerable: true, get: function () { return container_1.Container; } });
 var container_2 = require("./injection/default/container");
 Object.defineProperty(exports, "DefaultContainer", { enumerable: true, get: function () { return container_2.Container; } });
+__exportStar(require("./interfaces/database-orderbook"), exports);
+__exportStar(require("./interfaces/database-trade"), exports);
+__exportStar(require("./facades.d/admin"), exports);
+__exportStar(require("./facades.d/user-market"), exports);
+__exportStar(require("./facades.d/user-account"), exports);
+__exportStar(require("./texchange"), exports);
 //# sourceMappingURL=index.js.map
