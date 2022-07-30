b92485c0963dd2baccb79913e9f2195a5972a6a6
Zim <zimtsui@gmail.com>
2022-07-30 16:35:22 +0800

    makers injextends

--- build/models.d/makers/default.js
+++ build/models.d/makers/default.js
@@ -1,9 +1,16 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DefaultMakers = void 0;
 const secretary_like_1 = require("secretary-like");
 const makers_1 = require("./makers");
-class DefaultMakers extends makers_1.Makers {
+const injektor_1 = require("@zimtsui/injektor");
+let DefaultMakers = class DefaultMakers extends makers_1.Makers {
     // 默认单向持仓模式
     toFreeze(order) {
         if (order.action === secretary_like_1.Action.OPEN) {
@@ -33,6 +40,9 @@ class DefaultMakers extends makers_1.Makers {
             };
         }
     }
-}
+};
+DefaultMakers = __decorate([
+    (0, injektor_1.injextends)()
+], DefaultMakers);
 exports.DefaultMakers = DefaultMakers;
 //# sourceMappingURL=default.js.map
--- src/models.d/makers/default.ts
+++ src/models.d/makers/default.ts
@@ -8,7 +8,10 @@ import { Frozen } from '../../data-types/frozen';
 import { Balance } from '../../data-types/balance';
 import { Makers } from './makers';
 
+import { injextends } from '@zimtsui/injektor';
 
+
+@injextends()
 export class DefaultMakers<H extends HLike<H>> extends Makers<H> {
 	// 默认单向持仓模式
 	protected toFreeze(order: OpenOrder<H>): Frozen<H> {
